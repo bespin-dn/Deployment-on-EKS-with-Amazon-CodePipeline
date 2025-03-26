4fbde6569efcfa0bcae98f0705092c3a8aa31a06
geonsu.lee-at-603229842386 <ubuntu@GeonSu-Lee>
2025-03-26 13:21:18 +0900

    Init

--- routers/genai_service.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from services.bedrock_client import BedrockClient
-
-router = APIRouter()
-bedrock_client = BedrockClient()
-
-@router.post("/invoke")
-def invoke_bedrock_model(query: str):
-    return bedrock_client.invoke_model(query)
